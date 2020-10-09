0d2d9e5246a1ede5e91c8a49444c108c5c7077b7
Jacob Atchley <jatchley@firebend.com>
2020-10-09 16:46:15 -0500

    wip

--- Firebend.AutoCrud/Firebend.AutoCrud.Mongo.Sample/Models/Person.cs
+++ Firebend.AutoCrud/Firebend.AutoCrud.Mongo.Sample/Models/Person.cs
@@ -1,7 +1,7 @@
 using System;
 using Firebend.AutoCrud.Core.Interfaces.Models;
 
-namespace Firebend.AutoCrude.Mongo.Sample.Models
+namespace Firebend.AutoCrud.Mongo.Sample.Models
 {
     public class Person : IEntity<Guid>
     {
