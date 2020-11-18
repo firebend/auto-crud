6ed83361e36c2cf9ba199931a08c7542bf44d3e3
Jacob Atchley <yaaqob.atchley@gmail.com>
2020-11-18 16:46:02 -0600

    pet download example pet export view model

--- Firebend.AutoCrud.Web.Sample/Models/PetViewModel.cs
+++ Firebend.AutoCrud.Web.Sample/Models/PetViewModel.cs
@@ -2,6 +2,7 @@ using System;
 using System.ComponentModel.DataAnnotations;
 using Firebend.AutoCrud.Core.Extensions;
 using Firebend.AutoCrud.Core.Interfaces.Models;
+using Firebend.AutoCrud.Io.Attributes;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Firebend.AutoCrud.Web.Sample.Models
@@ -63,11 +64,34 @@ namespace Firebend.AutoCrud.Web.Sample.Models
         }
     }
 
-    public class ExportPetViewModel : PutPetViewModel
+    public class ExportPetViewModel
     {
+        [Export(Name = "Person Id", Order = 0)]
+        public Guid? PersonId { get; set; }
+
+        [Export(Name = "Person First Name", Order = 1)]
         public string PersonFirstName { get; set; }
 
+        [Export(Name = "Person Last  Name", Order = 2)]
         public string PersonLastName { get; set; }
+
+        [Export(Name = "Pet Id", Order = 3)]
+        public Guid PetId { get; set; }
+
+        [Export(Name = "Pet Name", Order = 4)]
+        public string PetName { get; set; }
+
+        [Export(Name = "Pet Type", Order = 5)]
+        public string PetType { get; set; }
+
+        [Export(Name = "Pet Created Date", Order = 6)]
+        public DateTimeOffset CreatedDate { get; set; }
+
+        [Export(Name = "Pet Modified Date", Order = 7)]
+        public DateTimeOffset ModifiedDate { get; set; }
+
+        [Export(Name = "Pet Deleted", Order = 8)]
+        public bool IsDeleted { get; set; }
 
         public ExportPetViewModel()
         {
@@ -79,6 +103,8 @@ namespace Firebend.AutoCrud.Web.Sample.Models
             pet.CopyPropertiesTo(this);
             PersonFirstName = pet.Person?.FirstName;
             PersonLastName = pet.Person?.LastName;
+            PersonId = pet.Person?.Id;
+            PetId = pet.Id;
         }
     }
 
