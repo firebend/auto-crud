6f2dee61d1d4ec4b02cc55ad088f860bcd2ca0e0
Jan Timpe <jtimpe@firebend.com>
2020-12-22 12:45:56 -0600

    AddCrud summaries

--- Firebend.AutoCrud.Core/Extensions/EntityBuilderExtensions/EntityBuilderConfiguratorExtensions.cs
+++ Firebend.AutoCrud.Core/Extensions/EntityBuilderExtensions/EntityBuilderConfiguratorExtensions.cs
@@ -7,6 +7,32 @@ namespace Firebend.AutoCrud.Core.Extensions.EntityBuilderExtensions
 {
     public static class EntityBuilderConfiguratorExtensions
     {
+        /// <summary>
+        /// Adds autocrud configuration for an entity
+        /// </summary>
+        /// <param name="configure">A callback allowing further configuration of the autocrud settings for an entity</param>
+        /// <example>
+        /// <code>
+        /// public static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args)
+        ///  .ConfigureWebHostDefaults(webbuilder => { webBuilder.UseStartup<Startup>(); })
+        ///  .ConfigureServices((hostContext, services) => {
+        ///      services.UsingMongoCrud("mongodb://localhost:27017", mongo => {
+        ///          mongo.AddEntity<Guid, WeatherForecast>(forecast =>
+        ///              forecast.WithDatabase("Samples")
+        ///                  .WithCollection("WeatherForecasts")
+        ///                  .WithFullTextSearch()
+        ///                  .AddCrud(x => x
+        ///                      .WithCrud()
+        ///                      // ... finish configuring CRUD for this entity
+        ///                   )
+        ///                  // ... finish configuring the entity
+        ///          )
+        ///      });
+        ///  })
+        ///  // ...
+        /// </code>
+        /// </example>
+        /// See <see cref="EntityCrudConfigurator"/> for additional configuration options available in the callback
         public static EntityCrudBuilder<TKey, TEntity> AddCrud<TKey, TEntity>(this EntityCrudBuilder<TKey, TEntity> builder,
             Action<EntityCrudConfigurator<EntityCrudBuilder<TKey, TEntity>, TKey, TEntity>> configure)
             where TKey : struct
@@ -17,6 +43,28 @@ namespace Firebend.AutoCrud.Core.Extensions.EntityBuilderExtensions
             return builder;
         }
 
+
+        /// <summary>
+        /// Adds basic autocrud configuration for an entity
+        /// </summary>
+        /// <example>
+        /// <code>
+        /// public static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args)
+        ///  .ConfigureWebHostDefaults(webbuilder => { webBuilder.UseStartup<Startup>(); })
+        ///  .ConfigureServices((hostContext, services) => {
+        ///      services.UsingMongoCrud("mongodb://localhost:27017", mongo => {
+        ///          mongo.AddEntity<Guid, WeatherForecast>(forecast =>
+        ///              forecast.WithDatabase("Samples")
+        ///                  .WithCollection("WeatherForecasts")
+        ///                  .WithFullTextSearch()
+        ///                  .AddCrud()
+        ///                  // ... finish configuring the entity
+        ///          )
+        ///      });
+        ///  })
+        ///  // ...
+        /// </code>
+        /// </example>
         public static EntityCrudBuilder<TKey, TEntity> AddCrud<TKey, TEntity>(this EntityCrudBuilder<TKey, TEntity> builder)
             where TKey : struct
             where TEntity : class, IEntity<TKey> => AddCrud(builder, crud => crud.WithCrud());
