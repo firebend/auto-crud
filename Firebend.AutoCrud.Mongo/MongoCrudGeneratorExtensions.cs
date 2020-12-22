5cb725bf0c4d24952b6299774e1c801a8ab9907d
Jan Timpe <jtimpe@firebend.com>
2020-12-22 12:16:59 -0600

    summary tags for MongoCurdGeneratorExtensions.UsingMongoCrud

--- Firebend.AutoCrud.Mongo/MongoCrudGeneratorExtensions.cs
+++ Firebend.AutoCrud.Mongo/MongoCrudGeneratorExtensions.cs
@@ -8,6 +8,25 @@ namespace Firebend.AutoCrud.Mongo
         public static MongoEntityCrudGenerator UsingMongoCrud(this IServiceCollection serviceCollection,
             string connectionString) => new MongoEntityCrudGenerator(serviceCollection, connectionString);
 
+
+        // <summary>
+        // Generates and adds configured Mongo entities to application's service collection
+        // </summary>
+        // <param name="connectionString">The connection string to your mongo database</param>
+        // <param name="configure">A callback function that allows configuring entities</param>
+        // <example>
+        // <code>
+        // public static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args)
+        //  .ConfigureWebHostDefaults(webbuilder => { webBuilder.UseStartup<Startup>(); })
+        //  .ConfigureServices((hostContext, services) => {
+        //      services.UsingMongoCrud("mongodb://localhost:27017", mongo => {
+        //          // ... configure entities
+        //      });
+        //  })
+        //  // ...
+        // </code>
+        // </example>
+        // See <see cref="MongoEntityCrudGenerator.AddEntity"/> for configuring entities
         public static IServiceCollection UsingMongoCrud(this IServiceCollection serviceCollection,
             string connectionString,
             Action<MongoEntityCrudGenerator> configure)
