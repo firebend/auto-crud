9490d8fe92fcf649a30b7252df6929e92ba08f6d
Jacob Atchley <yaaqob.atchley@gmail.com>
2021-01-27 15:26:37 -0600

    clean up config

--- Firebend.AutoCrud.Web.Sample.LoadTest/config.js
+++ Firebend.AutoCrud.Web.Sample.LoadTest/config.js
@@ -1,8 +1,5 @@
 module.exports = {
   localhost: {
-    BASE_URL: 'http://0.0.0.0:5000',
-    AUTH_ENDPOINT: 'http://0.0.0.0:5200/api/core/v1/authentication',
-    USERNAME: 'ct-support-persona@firebend.com',
-    PASSWORD: '7XWxcaJz@@gG8Z2V*8cqshqM',
+    BASE_URL: 'http://0.0.0.0:5000'
   }
 };
