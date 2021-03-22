72fd6265009371195fe2132fc25738b4b5da3d5f
Jacob Atchley <yaaqob.atchley@gmail.com>
2021-03-22 14:29:27 -0500

    fixes for service bus change tracking serialization package updates fixes for custom fields

--- Firebend.AutoCrud.Core/Models/DomainEvents/EntityUpdatedDomainEvent.cs
+++ Firebend.AutoCrud.Core/Models/DomainEvents/EntityUpdatedDomainEvent.cs
@@ -1,5 +1,9 @@
+using System.Collections.Generic;
 using Firebend.AutoCrud.Core.Extensions;
 using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.JsonPatch.Operations;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace Firebend.AutoCrud.Core.Models.DomainEvents
 {
@@ -7,20 +11,28 @@ namespace Firebend.AutoCrud.Core.Models.DomainEvents
         where T : class
     {
         private T _modified;
-
+        private string _operationsJson;
+        private List<Operation<T>> _operations;
         private JsonPatchDocument<T> _patch;
 
         public T Previous { get; set; }
 
-        public JsonPatchDocument<T> Patch
+        public string OperationsJson
         {
-            get => _patch;
+            get => _operationsJson;
             set
             {
-                _patch = value;
-                _modified = null;
+                _operationsJson = value;
+                _operations = null;
+                _patch = null;
             }
         }
+
+        public List<Operation<T>> Operations => _operations ??=
+            JsonConvert.DeserializeObject(OperationsJson, typeof(List<Operation<T>>),
+                new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All }) as List<Operation<T>>;
+
+        public JsonPatchDocument<T> Patch => _patch ??= new JsonPatchDocument<T>(Operations, new DefaultContractResolver());
 
         public T Modified
         {
