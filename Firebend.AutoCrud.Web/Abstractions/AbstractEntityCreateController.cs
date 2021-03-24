bfda33a957e8194c23066d56e7f47f9b118e83c5
Henry Darnell <hjdarnel@users.noreply.github.com>
2021-03-24 10:00:53 -0500

    Remove passive aggressive message 😀 (#54)

--- Firebend.AutoCrud.Web/Abstractions/AbstractEntityCreateController.cs
+++ Firebend.AutoCrud.Web/Abstractions/AbstractEntityCreateController.cs
@@ -34,7 +34,7 @@ namespace Firebend.AutoCrud.Web.Abstractions
 
         [HttpPost]
         [SwaggerOperation("Creates {entityNamePlural}")]
-        [SwaggerResponse(201, "A {entityName} was created successfully..")]
+        [SwaggerResponse(201, "A {entityName} was created successfully.")]
         [SwaggerResponse(400, "The request is invalid.")]
         [Produces("application/json")]
         public virtual async Task<ActionResult<TReadViewModel>> Post(
