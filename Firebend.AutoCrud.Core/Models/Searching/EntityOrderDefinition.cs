bcbe242869be4b4af009f99163f5f41744dec853
Jacob Atchley <yaaqob.atchley@gmail.com>
2021-09-17 15:05:15 -0500

    remove old file

--- Firebend.AutoCrud.Core/Models/Searching/EntityOrderDefinition.cs
+++
@@ -1,9 +0,0 @@
-namespace Firebend.AutoCrud.Core.Models.Searching
-{
-    public class EntityOrderDefinition
-    {
-        public string FieldName { get; set; }
-
-        public bool IsAscending { get; set; }
-    }
-}
