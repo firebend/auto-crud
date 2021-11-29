fd2759a3cd1687b7ec1705661a16b53fc8d75d03
enes cinar <enescihancinar@gmail.com>
2021-11-29 15:40:48 -0800

    auto mapper update (#76)
    * mappers & tests are added
    
    * propertiesToIgnore logic included
    
    * temp map key is updated with guid
    
    * new test case
    
    * dotnet format
    
    * test case name is updated
    
    * store ignored cases in the dict.
    
    Co-authored-by: Enes <ecinar@firebend.com>

---
+++ Firebend.AutoCrud.Core/ObjectMapping/BaseObjectMapper.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Firebend.AutoCrud.Core.ObjectMapping
+{
+    public abstract class BaseObjectMapper
+    {
+
+        protected abstract string MapTypes(Type source, Type target, params string[] propertiesToIgnore);
+        public abstract void Copy(object source, object target, params string[] propertiesToIgnore);
+
+        /// <summary>
+        /// This virtual function finds matching properties between given objects. It depends on their names, readability, and writability.
+        /// </summary>
+        /// <param name="sourceType">The source object's type</param>
+        /// <param name="targetType">The target object's type</param>
+        /// <returns>Returns a list of PropertyMap.</returns>
+        protected virtual IEnumerable<PropertyMap> GetMatchingProperties
+            (Type sourceType, Type targetType)
+        {
+            var sourceProperties = sourceType.GetProperties();
+            var targetProperties = targetType.GetProperties();
+
+            var properties = (from s in sourceProperties
+                              from t in targetProperties
+                              where s.Name == t.Name &&
+                                    s.CanRead &&
+                                    t.CanWrite
+                              select new PropertyMap
+                              {
+                                  SourceProperty = s,
+                                  TargetProperty = t
+                              }).ToList();
+            return properties;
+        }
+
+        /// <summary>
+        /// This virtual function builds a key to identify the converter method. It uses object names and combines them with defined pattern.
+        /// </summary>
+        /// <param name="sourceType">The source object type</param>
+        /// <param name="targetType">The target object type</param>
+        /// <returns>Returns a key as string</returns>
+        /// <exception cref="Exception">If there is no FullName property on sent objects, an exception will throw</exception>
+        protected virtual string GetMapKey(Type sourceType, Type targetType, params string[] propertiesToIgnore)
+        {
+            var keyName = "Copy_";
+
+            if (sourceType.FullName != null)
+            {
+                keyName += sourceType.FullName.Replace(".", "_").Replace("+", "_");
+                keyName += "_";
+            }
+            else
+            {
+                throw new Exception();
+            }
+
+            if (targetType.FullName != null)
+            {
+                keyName += targetType.FullName.Replace(".", "_").Replace("+", "_");
+            }
+            else
+            {
+                throw new Exception();
+            }
+
+            if (propertiesToIgnore != null)
+            {
+                keyName = propertiesToIgnore.Aggregate(keyName, (current, t) => current + $"_{t}");
+            }
+
+            return keyName;
+        }
+    }
+}
---
+++ Firebend.AutoCrud.Core/ObjectMapping/ObjectMapper.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+
+namespace Firebend.AutoCrud.Core.ObjectMapping
+{
+    /// <summary>
+    /// This mapper class finds the matching properties and copies them from source object to target object. The copy function has IL codes to do this task.
+    /// </summary>
+    public class ObjectMapper : BaseObjectMapper
+    {
+        /// <summary>
+        /// Easy access to mapper functions
+        /// </summary>
+        public static ObjectMapper Instance { get; protected set; } = new ObjectMapper();
+
+        private ObjectMapper() { }
+
+        /// <summary>
+        /// This dictionary keeps the convert functions for objects by auto generated names
+        /// </summary>
+        private readonly Dictionary<string, DynamicMethod> _del = new Dictionary<string, DynamicMethod>();
+
+        /// <summary>
+        /// This function creates the mappings between objects and store the mappings in the private dictionary
+        /// </summary>
+        /// <param name="source">The type of the source object</param>
+        /// <param name="target">The type of the target object</param>
+        /// <param name="propertiesToIgnore">These string parameters will be ignored during matching process</param>
+        /// <exception cref="InvalidOperationException">The Invalid Operation Exception will be thrown if it can't find the given property in source/target objects.</exception>
+        protected override string MapTypes(Type source, Type target, params string[] propertiesToIgnore)
+        {
+            var key = GetMapKey(source, target, propertiesToIgnore);
+
+            if (_del.ContainsKey(key))
+            {
+                return key;
+            }
+
+            var args = new[] { source, target };
+
+            var dm = new DynamicMethod(key, null, args);
+            var il = dm.GetILGenerator();
+            var maps = GetMatchingProperties(source, target);
+
+            foreach (var map in maps)
+            {
+                if (propertiesToIgnore.Contains(map.SourceProperty.Name))
+                {
+                    continue;
+                }
+
+                il.Emit(OpCodes.Ldarg_1);
+                il.Emit(OpCodes.Ldarg_0);
+                il.EmitCall(OpCodes.Callvirt, map.SourceProperty.GetGetMethod() ?? throw new InvalidOperationException(), null);
+                il.EmitCall(OpCodes.Callvirt, map.TargetProperty.GetSetMethod() ?? throw new InvalidOperationException(), null);
+            }
+            il.Emit(OpCodes.Ret);
+            _del.Add(key, dm);
+
+            return key;
+        }
+
+        /// <summary>
+        /// This function copies all matched property values from source object to target object
+        /// </summary>
+        /// <param name="source">The original object that keeps the actual values/properties</param>
+        /// <param name="target">The object that will get the related values from the given object</param>
+        /// <param name="propertiesToIgnore">These string parameters will be ignored during matching process</param>
+        public override void Copy(object source, object target, params string[] propertiesToIgnore)
+        {
+            var sourceType = source.GetType();
+            var targetType = target.GetType();
+
+            var key = this.MapTypes(sourceType, targetType, propertiesToIgnore);
+
+            var del = _del[key];
+            var args = new[] { source, target };
+            del.Invoke(null, args);
+        }
+    }
+}
