c30298778e33e5f689d52523bb325e9bcc413410
enes cinar <enescihancinar@gmail.com>
2022-02-25 12:06:20 -0800

    read all handler updated

--- Firebend.AutoCrud.Web.Sample/Authorization/Handlers/ReadAllAuthorizationHandler.cs
+++ Firebend.AutoCrud.Web.Sample/Authorization/Handlers/ReadAllAuthorizationHandler.cs
@@ -1,16 +1,17 @@
-using System.Reflection.Metadata;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Firebend.AutoCrud.Web.Implementations.Authorization.Requirements;
+using Firebend.AutoCrud.Web.Sample.Models;
 using Microsoft.AspNetCore.Authorization;
 
 namespace Firebend.AutoCrud.Web.Sample.Authorization.Handlers;
 
-public class ReadAllAuthorizationHandler : AuthorizationHandler<ReadAllAuthorizationRequirement, Document>
+public class ReadAllAuthorizationHandler : AuthorizationHandler<ReadAllAuthorizationRequirement, IEnumerable<IDataAuth>>
 {
     protected override Task HandleRequirementAsync(
         AuthorizationHandlerContext context,
         ReadAllAuthorizationRequirement requirement,
-        Document resource)
+        IEnumerable<IDataAuth> resource)
     {
         context.Succeed(requirement);
         return Task.CompletedTask;
