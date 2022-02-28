bbcaef34fc3c7895d40fc713fd742582c3af6cd3
Marcus Carey <mcarey@firebend.com>
2022-02-28 10:25:00 -0600

    Update EfEndToEndTest.cs to passing state

--- Firebend.AutoCrud.Web.Sample/Authorization/Handlers/CreateMultipleAuthorizationHandler.cs
+++ Firebend.AutoCrud.Web.Sample/Authorization/Handlers/CreateMultipleAuthorizationHandler.cs
@@ -1,16 +1,18 @@
+using System.Collections.Generic;
 using System.Reflection.Metadata;
 using System.Threading.Tasks;
 using Firebend.AutoCrud.Web.Implementations.Authorization.Requirements;
+using Firebend.AutoCrud.Web.Sample.Models;
 using Microsoft.AspNetCore.Authorization;
 
 namespace Firebend.AutoCrud.Web.Sample.Authorization.Handlers;
 
-public class CreateMultipleAuthorizationHandler : AuthorizationHandler<CreateMultipleAuthorizationRequirement, Document>
+public class CreateMultipleAuthorizationHandler : AuthorizationHandler<CreateMultipleAuthorizationRequirement, IDataAuth>
 {
     protected override Task HandleRequirementAsync(
         AuthorizationHandlerContext context,
         CreateMultipleAuthorizationRequirement requirement,
-        Document resource)
+        IDataAuth resource)
     {
         context.Succeed(requirement);
         return Task.CompletedTask;
