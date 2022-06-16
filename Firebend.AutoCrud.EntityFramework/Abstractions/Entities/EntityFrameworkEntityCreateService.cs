cefd30daca77dd7e46963e595df73f66100fcfe5
Marcus Carey <mcarey@firebend.com>
2022-06-16 13:31:53 -0500

    wip: Example of usage in crud service

--- Firebend.AutoCrud.EntityFramework/Abstractions/Entities/EntityFrameworkEntityCreateService.cs
+++ Firebend.AutoCrud.EntityFramework/Abstractions/Entities/EntityFrameworkEntityCreateService.cs
@@ -12,17 +12,25 @@ namespace Firebend.AutoCrud.EntityFramework.Abstractions.Entities
         where TEntity : class, IEntity<TKey>, new()
     {
         private readonly IEntityFrameworkCreateClient<TKey, TEntity> _createClient;
+        private readonly ISessionTransactionManager _transactionManager;
 
-        protected EntityFrameworkEntityCreateService(IEntityFrameworkCreateClient<TKey, TEntity> createClient)
+        protected EntityFrameworkEntityCreateService(IEntityFrameworkCreateClient<TKey, TEntity> createClient, ISessionTransactionManager transactionManager)
         {
             _createClient = createClient;
+            _transactionManager = transactionManager;
         }
 
-        public virtual Task<TEntity> CreateAsync(TEntity entity, CancellationToken cancellationToken = default)
-            => _createClient.AddAsync(entity, cancellationToken);
+        public virtual async Task<TEntity> CreateAsync(TEntity entity, CancellationToken cancellationToken = default)
+        {
+            var transaction = await _transactionManager.GetTransaction<TKey, TEntity>(cancellationToken);
+            return await _createClient.AddAsync(entity, transaction, cancellationToken);
+        }
 
         public Task<TEntity> CreateAsync(TEntity entity, IEntityTransaction transaction, CancellationToken cancellationToken = default)
-            => _createClient.AddAsync(entity, transaction, cancellationToken);
+        {
+            _transactionManager.AddTransaction(transaction);
+            return _createClient.AddAsync(entity, transaction, cancellationToken);
+        }
 
         protected override void DisposeManagedObjects() => _createClient?.Dispose();
     }
