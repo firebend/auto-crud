bb788ee7f0907fcb5e7d2d8c08d85c5eda2270f3
Terry Schneider <tnschneider@gmail.com>
2022-09-13 10:10:20 -0500

    feedback

--- Firebend.AutoCrud.Core/Implementations/Defaults/DefaultEntitySearchHandler.cs
+++ Firebend.AutoCrud.Core/Implementations/Defaults/DefaultEntitySearchHandler.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using System.Threading.Tasks;
 using Firebend.AutoCrud.Core.Interfaces.Models;
 using Firebend.AutoCrud.Core.Interfaces.Services.Entities;
 
@@ -11,12 +12,18 @@ public class DefaultEntitySearchHandler<TKey, TEntity, TSearch> : IEntitySearchH
     where TSearch : IEntitySearchRequest
 {
     private readonly Func<IQueryable<TEntity>, TSearch, IQueryable<TEntity>> _func;
+    private readonly Func<IQueryable<TEntity>, TSearch, Task<IQueryable<TEntity>>> _asyncFunc;
 
-    public DefaultEntitySearchHandler(Func<IQueryable<TEntity>, TSearch, IQueryable<TEntity>> func)
+    public DefaultEntitySearchHandler(Func<IQueryable<TEntity>, TSearch, IQueryable<TEntity>> func,
+        Func<IQueryable<TEntity>, TSearch, Task<IQueryable<TEntity>>> asyncFunc = null)
     {
         _func = func;
+        _asyncFunc = asyncFunc;
     }
 
     public IQueryable<TEntity> HandleSearch(IQueryable<TEntity> queryable, TSearch searchRequest) =>
         _func == null ? queryable : _func(queryable, searchRequest);
+
+    public Task<IQueryable<TEntity>> HandleSearchAsync(IQueryable<TEntity> queryable, TSearch searchRequest) =>
+        _asyncFunc == null ? Task.FromResult(queryable) : _asyncFunc(queryable, searchRequest);
 }
