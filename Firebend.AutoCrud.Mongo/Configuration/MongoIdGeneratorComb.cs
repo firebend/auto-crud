1f7af3146f44c8fe77643b2c50b229392b36f1c6
Jacob Atchley <yaaqob.atchley@gmail.com>
2023-04-19 18:41:52 -0500

    chore: moved comb guid functionality to core

--- Firebend.AutoCrud.Mongo/Configuration/MongoIdGeneratorComb.cs
+++ Firebend.AutoCrud.Mongo/Configuration/MongoIdGeneratorComb.cs
@@ -1,4 +1,5 @@
 using System;
+using Firebend.AutoCrud.Core.Ids;
 using MongoDB.Bson.Serialization;
 
 namespace Firebend.AutoCrud.Mongo.Configuration
@@ -7,28 +8,9 @@ namespace Firebend.AutoCrud.Mongo.Configuration
     {
         public static readonly MongoIdGeneratorComb Instance = new();
 
-        public object GenerateId(object container, object document) => NewCombGuid(Guid.NewGuid(), DateTime.UtcNow);
+        public object GenerateId(object container, object document)
+            => CombGuid.New();
 
         public bool IsEmpty(object id) => id == default || (Guid)id == Guid.Empty;
-
-        public static Guid NewCombGuid(Guid guid, DateTime timestamp)
-        {
-            var dateTime = DateTime.UnixEpoch;
-            var timeSpan = timestamp - dateTime;
-            var timeSpanMs = (long)timeSpan.TotalMilliseconds;
-            var timestampString = timeSpanMs.ToString("x8");
-            var guidString = guid.ToString("N");
-
-            var newGuidString = $"{timestampString[..11]}{guidString[11..]}";
-
-            if (string.IsNullOrWhiteSpace(newGuidString))
-            {
-                throw new Exception("Could not get guid string");
-            }
-
-            var newGuid = Guid.Parse(newGuidString);
-
-            return newGuid;
-        }
     }
 }
