adb343c4fffd88ab0157f62702662e89ed2000c6
Jacob Atchley <yaaqob.atchley@gmail.com>
2023-05-18 11:15:24 -0500

    remove comment code

--- Firebend.AutoCrud.Web.Sample/Authorization/Handlers/ReadAllAuthorizationHandler.cs
+++ Firebend.AutoCrud.Web.Sample/Authorization/Handlers/ReadAllAuthorizationHandler.cs
@@ -8,31 +8,11 @@ namespace Firebend.AutoCrud.Web.Sample.Authorization.Handlers;
 
 public class ReadAllAuthorizationHandler : AuthorizationHandler<ReadAllAuthorizationRequirement, IEnumerable<IEntityDataAuth>>
 {
-    // private readonly DataAuthService _dataAuthService;
-    //
-    // public ReadAllAuthorizationHandler(DataAuthService dataAuthService)
-    // {
-    //     _dataAuthService = dataAuthService;
-    // }
-
     protected override Task HandleRequirementAsync(
         AuthorizationHandlerContext context,
         ReadAllAuthorizationRequirement requirement,
         IEnumerable<IEntityDataAuth> resources)
     {
-        // var entityDataAuths = resources as IEntityDataAuth[] ?? resources.ToArray();
-        // // ReSharper disable once CollectionNeverQueried.Local
-        // var authorizedResources = new List<IEntityDataAuth>();
-        //
-        // foreach (var entityDataAuth in entityDataAuths)
-        // {
-        //     var isAuthorized = await _dataAuthService.AuthorizeAsync(context.User, entityDataAuth.DataAuth);
-        //     if (!isAuthorized)
-        //     {
-        //         authorizedResources.Add(entityDataAuth);
-        //     }
-        // }
-
         // TODO need to change response body
 
         return Task.CompletedTask;
