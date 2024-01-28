7efee8a0ddb3af326e9d775b08aa02ab6dbc60bd
Jacob Atchley <yaaqob.atchley@gmail.com>
2024-01-28 00:19:53 -0600

    fix: perf is empty on collections

--- Firebend.AutoCrud.Core/Extensions/EnumerableExtensions.cs
+++ Firebend.AutoCrud.Core/Extensions/EnumerableExtensions.cs
@@ -5,9 +5,15 @@ namespace Firebend.AutoCrud.Core.Extensions;
 
 public static class EnumerableExtensions
 {
-    public static bool IsEmpty<T>(this IEnumerable<T> source) => source == null || !source.Any();
+    public static bool IsEmpty<T>(this IEnumerable<T> source)
+        => source switch
+        {
+            null => true,
+            ICollection<T> collection => collection.Count == 0,
+            _ => source.Any()
+        };
 
-    public static bool HasValues<T>(this IEnumerable<T> source) => source != null && source.Any();
+    public static bool HasValues<T>(this IEnumerable<T> source) => source.IsEmpty() is false;
 
     public static IEnumerable<T> NullCheck<T>(this IEnumerable<T> source) => source ?? Enumerable.Empty<T>();
 }
