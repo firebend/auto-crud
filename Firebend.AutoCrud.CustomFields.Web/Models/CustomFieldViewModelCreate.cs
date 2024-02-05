e1afca5e3b90bd39a7825e82bb9883e7499b005e
Jacob Atchley <yaaqob.atchley@gmail.com>
2024-02-05 11:34:03 -0600

    fix: reduce custom fields max length

--- Firebend.AutoCrud.CustomFields.Web/Models/CustomFieldViewModelCreate.cs
+++ Firebend.AutoCrud.CustomFields.Web/Models/CustomFieldViewModelCreate.cs
@@ -5,10 +5,10 @@ namespace Firebend.AutoCrud.CustomFields.Web.Models;
 public class CustomFieldViewModelCreate
 {
     [MinLength(1)]
-    [MaxLength(255)]
+    [MaxLength(250)]
     public string Key { get; set; }
 
     [MinLength(1)]
-    [MaxLength(1000)]
+    [MaxLength(250)]
     public string Value { get; set; }
 }
